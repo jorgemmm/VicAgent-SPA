cdef5fbd89d8fa8f9cf609ae120cb04b90348575
jorgemmm <vj_plasin@hotmail.com>
2021-11-03 16:58:12 +0000

    cambios en rouitg y style y share

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -10,28 +10,6 @@ import {HeroeDetailComponent} from './components/heroe-detail/heroe-detail.compo
 import { SearchComponent } from './components/search/search.component';
 import { PrivacyComponent} from './components/privacy/privacy.component'
 
-// import { Name4Component } from './';
-// import { PageNotFoundComponent } from './';
-
-// const routes: Routes = [
-//   { path: '', component: HomeComponent },
-//   { path: 'path2', component: Name2Component },
-//   { path: 'path3', component: Name3Component },
-//   { path: 'path4', component: Name4Component },
-//   { path: '**', component: PageNotFoundComponent },
-
-  //{ path: 'path/:routeParam', component: MyComponent },
-  //{ path: 'staticPath', component: ... },
-  //{ path: '**', component: ... },
-  //{ path: 'oldPath', redirectTo: '/staticPath' },
-  //{ path: ..., component: ..., data: { message: 'Custom' }
-  //];
-
-// @NgModule({
-//   imports: [RouterModule.forChild(routes)],
-//   exports: [RouterModule]
-// })
-
 export class FeatureRoutingModule {}
 
 
@@ -39,8 +17,7 @@ const APP_ROUTES: Routes = [
   { path: 'home', component: HomeComponent },
   { path: 'about', component: AboutComponent },
   { path: 'privacy', component: PrivacyComponent },
-  { path: 'heroes', component: HeroesComponent },
-  
+  { path: 'heroes', component: HeroesComponent },  
   { path: 'games', component: HeroesComponent },
 
   // { path: 'games', component: GamesComponent },
